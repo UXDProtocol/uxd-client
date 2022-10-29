0932038896b817b9c71d719d9b411411323589a7
Grégory NEUT <gregory.neut@gmail.com>
2022-10-29 09:17:48 +0400

    format

--- src/mercurial/types.d.ts
+++ src/mercurial/types.d.ts
@@ -1,28 +1,26 @@
+import { Vault as MercurialVaultIDL } from './vaultIdl';
 import {
-    Vault as MercurialVaultIDL,
-} from './vaultIdl';
-import {
-    IdlTypes,
-    TypeDef,
+  IdlTypes,
+  TypeDef,
 } from '@project-serum/anchor/dist/cjs/program/namespace/types';
 
 // HELPER FUNCTIONS COPY PASTED FROM MERCURIAL VAULT NPM PACKAGE
 // WE DO NOT WANT TO INCLUDE THE WHOLE MERCURIAL VAULT NPM PACKAGE
 // DUE TO DEPENDENCIES ISSUES
 export interface ParsedClockState {
-    info: {
-        epoch: number;
-        epochStartTimestamp: number;
-        leaderScheduleEpoch: number;
-        slot: number;
-        unixTimestamp: number;
-    };
-    type: string;
-    program: string;
-    space: number;
+  info: {
+    epoch: number;
+    epochStartTimestamp: number;
+    leaderScheduleEpoch: number;
+    slot: number;
+    unixTimestamp: number;
+  };
+  type: string;
+  program: string;
+  space: number;
 }
 
 export type VaultState = TypeDef<
-    MercurialVaultIDL['accounts']['0'],
-    IdlTypes<MercurialVaultIDL>
+  MercurialVaultIDL['accounts']['0'],
+  IdlTypes<MercurialVaultIDL>
 >;
