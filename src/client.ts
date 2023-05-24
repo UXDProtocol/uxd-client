4f400e94a9d663519d4550ba7aa27b7487d163c2
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-05-24 16:36:14 +0900

    updating-client-and-dependencies

--- src/client.ts
+++ src/client.ts
@@ -681,9 +681,10 @@ export class UXDClient {
     mercurialVaultDepository: MercurialVaultDepository,
     credixLpDepository: CredixLpDepository,
     payer: PublicKey,
-    profitsBeneficiaryCollateral: PublicKey,
     options: ConfirmOptions
   ): TransactionInstruction {
+    const collateralMintPda = identityDepository.collateralMint;
+
     return this.instruction.rebalanceRequestCreateFromCredixLpDepository({
       accounts: {
         payer: payer,
@@ -691,23 +692,60 @@ export class UXDClient {
         identityDepository: identityDepository.pda,
         mercurialVaultDepository: mercurialVaultDepository.pda,
         depository: credixLpDepository.pda,
-        depositoryCollateral: depository.depositoryCollateral,
-        depositoryShares: depository.depositoryShares,
-        collateralMint: depository.collateralMint,
-        credixProgramState: depository.credixProgramState,
-        credixGlobalMarketState: depository.credixGlobalMarketState,
-        credixSigningAuthority: depository.credixSigningAuthority,
-        credixLiquidityCollateral: depository.credixLiquidityCollateral,
-        credixSharesMint: depository.credixSharesMint,
-        credixPass: depository.credixPass,
-        credixTreasuryCollateral: depository.credixTreasuryCollateral,
-        credixMultisigKey: depository.credixMultisigKey,
-        credixMultisigCollateral: depository.credixMultisigCollateral,
+        collateralMint: collateralMintPda,
+        depositoryShares: credixLpDepository.depositoryShares,
+        credixGlobalMarketState: credixLpDepository.credixGlobalMarketState,
+        credixSigningAuthority: credixLpDepository.credixSigningAuthority,
+        credixLiquidityCollateral: credixLpDepository.credixLiquidityCollateral,
+        credixSharesMint: credixLpDepository.credixSharesMint,
+        credixPass: credixLpDepository.credixPass,
+        credixWithdrawEpoch: credixLpDepository.credixWithdrawEpoch,
+        credixWithdrawRequest: credixLpDepository.credixWithdrawRequest,
+        systemProgram: SystemProgram.programId,
+        credixProgram: credixLpDepository.credixProgramId,
+      },
+      options,
+    });
+  }
+
+  public createRebalanceRequestExecuteFromCredixLpDepositoryInstruction(
+    controller: Controller,
+    identityDepository: IdentityDepository,
+    mercurialVaultDepository: MercurialVaultDepository,
+    credixLpDepository: CredixLpDepository,
+    payer: PublicKey,
+    profitsBeneficiaryCollateral: PublicKey,
+    options: ConfirmOptions
+  ): TransactionInstruction {
+    const collateralMintPda = identityDepository.collateralMint;
+
+    return this.instruction.rebalanceRequestExecuteFromCredixLpDepository({
+      accounts: {
+        payer: payer,
+        controller: controller.pda,
+        identityDepository: identityDepository.pda,
+        identityDepositoryCollateral: identityDepository.collateralVaultPda,
+        mercurialVaultDepository: mercurialVaultDepository.pda,
+        depository: credixLpDepository.pda,
+        collateralMint: collateralMintPda,
+        depositoryCollateral: credixLpDepository.depositoryCollateral,
+        depositoryShares: credixLpDepository.depositoryShares,
+        credixProgramState: credixLpDepository.credixProgramState,
+        credixGlobalMarketState: credixLpDepository.credixGlobalMarketState,
+        credixSigningAuthority: credixLpDepository.credixSigningAuthority,
+        credixLiquidityCollateral: credixLpDepository.credixLiquidityCollateral,
+        credixSharesMint: credixLpDepository.credixSharesMint,
+        credixPass: credixLpDepository.credixPass,
+        credixTreasuryCollateral: credixLpDepository.credixTreasuryCollateral,
+        credixMultisigKey: credixLpDepository.credixMultisigKey,
+        credixMultisigCollateral: credixLpDepository.credixMultisigCollateral,
+        credixWithdrawEpoch: credixLpDepository.credixWithdrawEpoch,
+        credixWithdrawRequest: credixLpDepository.credixWithdrawRequest,
         profitsBeneficiaryCollateral: profitsBeneficiaryCollateral,
         systemProgram: SystemProgram.programId,
-        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
         tokenProgram: TOKEN_PROGRAM_ID,
-        credixProgram: depository.credixProgramId,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        credixProgram: credixLpDepository.credixProgramId,
         rent: SYSVAR_RENT_PUBKEY,
       },
       options,
